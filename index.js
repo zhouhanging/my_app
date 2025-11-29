01050a583432b4fbcd2fd51816dc58568d5085f9
zhouhang <3493329756@qq.com>
2025-11-29 13:27:22 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+
+// 创建窗口的方法
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+  win.loadFile("index.html");
+};
+
+// whenReady是一个生命周期方法，会在 Electron 完成应用初始化后调用
+// 返回一个 promise
+app.whenReady().then(() => {
+  createWindow();
+});
+
